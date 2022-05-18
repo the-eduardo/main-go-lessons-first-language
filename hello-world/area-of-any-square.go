fd64605d569632131f6682ef4b931a6bc155a026
the-eduardo <eduardo.xbox@live.com>
2022-05-17 23:32:09 -0300

    undo**

--- hello-world/area-of-any-square.go
+++ hello-world/area-of-any-square.go
@@ -7,12 +7,11 @@ import ("fmt")
 
   func main () {
     var x float64
-    var abc string
 
     fmt.Printf("Please enter content:")
     fmt.Scan(&x)
     y := x
     area, perimetro := square(x, y)
-    fmt.Print("Your has square area is " area, " meters\n", "And has a perimeter of ", perimetro, " meters")
+    fmt.Print("Your has square area is ", area, " meters\n", "And has a perimeter of ", perimetro, " meters")
 
   }
