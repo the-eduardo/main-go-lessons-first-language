b8e018fb24fe534443205e765ee07c42e7d18575
the-eduardo <eduardo.xbox@live.com>
2022-05-18 13:16:42 -0300

    TODO - Add a variable that keeps track of which planet your spaceship is on. - Create a function that returns your spaceship back to your home planet. - Add more destinations and allow for traveling between different planets.

--- solar-travel/travel.go
+++ solar-travel/travel.go
@@ -21,6 +21,13 @@ func calculateFuel(planet string) int {
 	return (fuel)
 }
 
+/** TODO
+- Add a variable that keeps track of which planet your spaceship is on.
+- Create a function that returns your spaceship back to your home planet.
+- Add more destinations and allow for traveling between different planets.
+ **/
+
+
 func greetPlanet(planet string) {
 	fmt.Println("Welcome to Planet", planet)
 }
@@ -44,8 +51,10 @@ func flyToPlanet(planet string, fuel int) int {
 }
 
 func main() {
-	var fuel int = 500000
-	var planetChoice string = "Venus"
+	var fuel int = 100000
+	var planetChoice string
+	fmt.Print("Select a planet to fly: \n", "Venus, Mercury, Mars\n")
+	fmt.Scan(&planetChoice)
 	fuel = flyToPlanet(planetChoice, fuel)
 	fuelGauge(fuel)
 }
