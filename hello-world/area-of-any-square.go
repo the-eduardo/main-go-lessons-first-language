8ced1f6aaeaae24cddf565f8ff439a0482dbb5f9
the-eduardo <eduardo.xbox@live.com>
2022-05-17 22:39:50 -0300

    syntax test

--- hello-world/area-of-any-square.go
+++ hello-world/area-of-any-square.go
@@ -12,6 +12,6 @@ import ("fmt")
     fmt.Scan(&x)
     y := x
     area, perimetro := square(x, y)
-    fmt.Print("Your has square area is ", area, " meters\n", "And has a perimeter of ", perimetro, " meters")
+    fmt.Print("Your has square area is " area, " meters\n", "And has a perimeter of ", perimetro, " meters")
 
   }
