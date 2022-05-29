13a41522ed434080ec2cb35a9641672c31bc3ffc
eduardo <eduardo.xbox@live.com>
2022-05-29 20:11:08 -0300

    https://pkg.go.dev/fmt

---
+++ sistemas-numericos/tests.go
@@ -0,0 +1,27 @@
+package main
+import ("fmt"
+  "runtime")
+
+func main() {
+  s := "hello world!"
+  sb := []byte(s)
+  for _, v := range sb {
+    fmt.Printf("%v | %T | %#U | %#x\n", v, v, v, v)
+  }
+  a := 700000000008745
+  fmt.Printf("\n\n\n %d \t %b \t %#x", a ,a ,a )
+  fmt.Println("\n", runtime.GOOS, "|", runtime.GOARCH)
+}
+/* https://pkg.go.dev/fmt
+Integer:
+
+%b	base 2
+%c	the character represented by the corresponding Unicode code point
+%d	base 10
+%o	base 8
+%O	base 8 with 0o prefix
+%q	a single-quoted character literal safely escaped with Go syntax.
+%x	base 16, with lower-case letters for a-f
+%X	base 16, with upper-case letters for A-F
+%U	Unicode format: U+1234; same as "U+%04X"
+**/
