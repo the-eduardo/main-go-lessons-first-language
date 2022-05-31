d0593b1a6b0857eb55298fc5e67667894f3fe199
eduardo <eduardo.xbox@live.com>
2022-05-31 02:42:40 -0300

    added Scan to Collatz Conjecture

---
+++ Collatz-Conjecture/Collatz-Conjecture.test.go
@@ -0,0 +1,20 @@
+// https://www.youtube.com/watch?v=094y1Z2wpJg&t=1098s
+
+//a number is odd *3+1
+// if even /2
+
+package main
+import "fmt"
+func main() {
+  var sum uint64
+  fmt.Scan(&sum)
+	for ; sum > 1; {
+		if sum&1 != 0 {
+			sum = sum*3 + 1
+			fmt.Printf("%v = ODD\n", sum)
+		} else {
+			sum = sum / 2
+			fmt.Printf("%v = EVEN\n", sum)
+		}
+	}
+}
