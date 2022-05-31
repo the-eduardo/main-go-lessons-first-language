9545fcd534df2c5622626ee0751868384697f5b3
eduardo <eduardo.xbox@live.com>
2022-05-31 02:20:33 -0300

    Working Collatz Conjecture in Go

--- Collatz-Conjecture/Collatz-Conjecture.go
+++ Collatz-Conjecture/Collatz-Conjecture.go
@@ -4,43 +4,16 @@
 // if even /2
 
 package main
-import ("fmt"
-    "math/rand"
-    "time"
-  )
-
-  func oddEven(num int) int{
-   if num & 1 != 0 {
-      fmt.Println("ODD")
-      num = num*3+1
-      fmt.Println(num)
-      return num
-   } else {
-      fmt.Println("EVEN")
-      num = num/2
-      fmt.Println(num)
-      return num
-   }
-}
-
-func main(){
-  var sum int
-  rand.Seed(time.Now().UnixNano())
-
-  oddEven(rand.Intn(100))
-  oddEven(rand.Intn(100))
-  oddEven(rand.Intn(100))
-
-  sum = (rand.Intn(100))
-  oddEven(sum)
-  if sum & 1 != 0 {
-     fmt.Println("ODD ***")
-     sum = sum*3+1
-     fmt.Println(sum)
-  } else {
-     fmt.Println("EVEN ---")
-     sum = sum/2
-     fmt.Println(sum)
-  }
-
+import "fmt"
+func main() {
+  var sum uint64
+	for sum = 27; sum > 1; {
+		if sum&1 != 0 {
+			sum = sum*3 + 1
+			fmt.Printf("%v = ODD\n", sum)
+		} else {
+			sum = sum / 2
+			fmt.Printf("%v = EVEN\n", sum)
+		}
+	}
 }
