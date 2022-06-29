12852ed9e065c07593ee72d6f20a01c2f0432610
eduardo <eduardo.xbox@live.com>
2022-06-29 01:18:10 -0300

    added multiple practices

--- hello-world/Aprenda-Go/practices-level-06-4.go
+++ hello-world/Aprenda-Go/practices-level-06-4.go
@@ -2,17 +2,17 @@ package main
 
 import "fmt"
 
-type person struct {
+type persona struct {
 	name, nick string
 	age        int
 }
 
-func (p person) sayhi() {
+func (p persona) sayhi() {
 	fmt.Println("Hi, I'm", p.name, p.nick, ",I'm", p.age, "years old.")
 }
 func main() {
 
-	a := person{name: "John", nick: "Paulman", age: 40}
+	a := persona{name: "John", nick: "Paulman", age: 40}
 
 	a.sayhi()
 }
