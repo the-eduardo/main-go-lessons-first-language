9e5368f4a14fbb52e9076663471f211cf9c412ca
eduardo <eduardo.xbox@live.com>
2022-07-04 08:44:04 -0300

    added mutable/immutable data examples

---
+++ hello-world/Tech With Tim/18_Mutable&ImmutableDataTypes.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	func() {
+		var x int = 5
+		y := &x
+		*y = 10
+		fmt.Println("With Pointers: x =", x, "| y =", *y)
+	}()
+	func() {
+		var x int = 5
+		y := x
+		y = 10
+		fmt.Println("W.No Pointers: x =", x, "| y =", y)
+	}()
+	fmt.Println("------------------------------------------------------")
+	func() {
+		var x = []int{1, 2, 3, 4, 5}
+		y := x
+		fmt.Println("x == y", x, y)
+		y[1] = 50
+		fmt.Println("y is x", x, y)
+	}()
+}
