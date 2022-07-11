7e6a632800d1201cf5b15f38d16b585c980786e6
eduardo <eduardo.xbox@live.com>
2022-07-11 20:33:34 -0300

    New Feature: Prints the higher value

--- Collatz-Conjecture/Collatz-Conjecture.go
+++ Collatz-Conjecture/Collatz-Conjecture.go
@@ -8,14 +8,21 @@ package main
 import "fmt"
 
 func main() {
-	var sum uint64
+	var sum, max uint64
 	for sum = 27; sum > 1; {
 		if sum&1 != 0 {
 			sum = sum*3 + 1
+				if sum > max { // New Feature: Prints the higher value
+					max = sum
+				}
 			fmt.Printf("%v = ODD\n", sum)
 		} else {
 			sum = sum / 2
+			if sum > max {
+				max = sum
+			}
 			fmt.Printf("%v = EVEN\n", sum)
 		}
 	}
+	fmt.Println("\nmax value:",max)
 }
